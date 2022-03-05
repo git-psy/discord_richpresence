e9f9567d4c515e5e94017e669a106af11d6e806c
gitpsy <perdu.felix@free.fr>
2022-03-05 15:53:06 +0100

    version: 1

---
+++ richpresence.py
@@ -0,0 +1,36 @@
+import time
+from pypresence import Presence
+
+client_id = "926421445213552702"
+rpc = Presence(client_id=client_id)
+def test():
+    try:
+        try:
+            rpc.update(buttons=[
+                {"label": "Mon site",
+                 "url": "https://git-psy.github.io/index.html"},
+                {"label": "Mon Github",
+                 "url": "https://github.com/git-psy/"}],
+                details="Bijour,",
+                state="Et toi, ca vas ?",
+                small_image="small_image",
+                small_text="Ca c'est spotify",
+                large_image = "large_image",
+                large_text="Html, le language de base pour créer un site web...")
+        except:
+            rpc.connect()
+            rpc.update(buttons=[
+                {"label": "Mon site",
+                 "url": "https://git-psy.github.io/index.html"},
+                {"label": "Mon Github",
+                 "url": "https://github.com/git-psy/"}],
+                details="Bijour,",
+                state="Et toi, ca vas ?",
+                small_image="small_image",
+                small_text="Ca c'est spotify",
+                large_image = "large_image",
+                large_text="Html, le language de base pour créer un site web...")
+    except:pass
+while 1:
+    test()
+    time.sleep(15)
